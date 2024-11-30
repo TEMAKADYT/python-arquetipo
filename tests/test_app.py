d53e5f1b20544ae5602e03545934e5da49d60e57
koerhunt <eliasdevsanchez@gmail.com>
2024-11-30 16:19:39 -0600

    test app base

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+# import pytest
+# from origamiapp.server import app
+
+# @pytest.mark.asyncio
+# async def test_graphql_setup(application):
+
+#     request, response = app.test_client.post('/read-api')
+
+#     assert request.method.lower() == "get"
+#     assert response.body == b"foo"
+#     assert response.status == 200
